22def4d76c6a37858fc483cb499ecd0a1101d700
Grishel-001 <grishelsonalimascarenhas@gmail.com>
2025-12-11 10:58:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+from requests.exceptions import ConnectionError, Timeout
+
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=2e01598309ce436b0193dd67cb54a059&language=en-US"
+
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+
+        poster_path = data.get('poster_path')
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+    except (ConnectionError, Timeout):
+        return "https://via.placeholder.com/500x750?text=Error+Loading"
+
+    except Exception:
+        return "https://via.placeholder.com/500x750?text=Unknown+Error"
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movie_posters = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movie_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, recommended_movie_posters
+
+
+# Load files
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+st.title("Movie Recommender System")
+
+selected_movie_name = st.selectbox("Select a movie", movies['title'].values)
+
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie_name)
+
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
